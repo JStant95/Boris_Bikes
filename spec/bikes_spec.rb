df69e641dec65655f1d79ff5fdbce08871d75ace
Ed <lookatemail@gmail.com>
2020-04-15 15:14:32 +0100

    step 8

--- spec/bikes_spec.rb
+++ spec/bikes_spec.rb
@@ -2,5 +2,10 @@
 require 'bikes'
 
 describe DockingStation do
+
+  it 'instance of the DockingStation should respond to release_bike' do
+    bike = DockingStation.new
+    expect(bike).to respond_to(:release_bike)
+  end
   
 end
