91f2abca1412940d8b7f287197a848f1ea4799f0
Ed <lookatemail@gmail.com>
2020-04-15 16:31:58 +0100

    step 10 Walkthrough - making Docking Stations get Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
+require 'bikes'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it 'release working bikes' do
+    # dock = DockingStation.new
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
