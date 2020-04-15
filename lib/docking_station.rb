5433c40d8782d41e684254331efd70463bfca171
Ed <lookatemail@gmail.com>
2020-04-15 17:17:31 +0100

    10.35 done ...

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,20 @@
 class DockingStation
+  attr_reader :bike 
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+     @bike = bike
+  end
 end
+
+# station1 = DockingStation.new
+# bike1 = Bike.new
+# station1.dock(bike1)
+
+read = DockingStation.new
+
+p read.dock("Boris bike")
+p read.bike
