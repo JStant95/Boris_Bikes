1d48653f61653943e78757e6d52d5906dc306177
Jed Stanton <jedstanton@hotmail.com>
2020-04-16 14:37:52 +0100

    Chapter 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,11 @@ require 'docking_station'
 require 'bikes'
 
 describe DockingStation do
-  # Step 8 Unit test
+  subject (:docking) {described_class.new}
   it { is_expected.to respond_to :release_bike}
 
   it 'release working bikes' do
-    # dock = DockingStation.new
-    bike = subject.release_bike
-    expect(bike).to be_working
+    expect(docking.release_bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
