46f5125cf47b42fa2f30eadcd5002c75ae618054
Jed Stanton <jedstanton@hotmail.com>
2020-04-16 16:23:20 +0100

    challenge 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,12 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail "There are no bikes in this station" unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'There is already a bike in this station' unless @bike == nil
     @bike = bike
   end
-
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,27 @@ describe DockingStation do
   subject (:docking) {described_class.new}
   it { is_expected.to respond_to :release_bike}
 
-  it 'release working bikes' do
-    expect(docking.release_bike).to be_working
-  end
+it 'release working bikes' do
+bike = Bike.new
+docking.dock(bike)
+expect(docking.release_bike).to be_working
+end
 
  it { is_expected.to respond_to(:dock).with(1).argument }
 
  it { is_expected.to respond_to :bike}
+
+  it 'checking' do
+    if @bike == nil
+      expect { docking.release_bike }.to raise_error("There are no bikes in this station")
+    end
+  end
+
+  it 'capacity checking' do
+    docking.dock(Bike.new)
+
+    expect { docking.dock(Bike.new) }.to raise_error("There is already a bike in this station")
+
+
+  end
 end
